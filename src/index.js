99fe81aca777449b5f502d93430aa42fc1d1a6bd
AFLP2199 <fernandopiceda99@gmail.com>
2023-02-07 14:00:30 -0300

    update fee

--- src/index.js
+++ src/index.js
@@ -1,90 +1,90 @@
-const calcular = document.querySelector("#button");
-const monto = document.querySelector("#monto");
-const comisionOntop = 1.54;
-const comisionCrypto = 2;
+const calcular = document.querySelector("#button")
+const monto = document.querySelector("#monto")
+const comisionOntop = 1.54
+const comisionCrypto = 2
 
-async function getDolarCCL() {
+async function getDolarCCL () {
   try {
     const response = await fetch(
       "https://mercados.ambito.com//dolarrava/cl/variacion",
       { method: "GET" }
-    );
-    var data = await response.json();
-    var dolarCCL = parseFloat(data.compra.replace(",", "."));
-    let montoACambiar = parseFloat(monto.value);
-    var totalOntop = (montoACambiar - comisionOntop) * dolarCCL * 0.93;
+    )
+    var data = await response.json()
+    var dolarCCL = parseFloat(data.compra.replace(",", "."))
+    let montoACambiar = parseFloat(monto.value)
+    var totalOntop = (montoACambiar - comisionOntop) * dolarCCL * 0.885
     if (!montoACambiar || Number.isNaN(montoACambiar) || montoACambiar < 20) {
       document.querySelector("#total-ontop").innerText = `
-      El monto minimo para transferir desde Ontop es $20`;
-      return null;
+      El monto minimo para transferir desde Ontop es $20`
+      return null
     }
     document.querySelector("#total-ontop").innerText = `
               Dolar CCL = $${dolarCCL}
               TOTAL = $${totalOntop.toFixed(2)}
               Estarias cambiando a = $${(
-                totalOntop / montoACambiar
-              ).toFixed(2)}
-              `;
+        totalOntop / montoACambiar
+      ).toFixed(2)}
+              `
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-async function getDolarCrypto() {
+async function getDolarCrypto () {
   try {
     const response = await fetch(
       `https://criptoya.com/api/lemoncash/USDC`,
       { method: "GET" }
-    );
-    var data = await response.json();
-    var montoACambiar = parseFloat(monto.value);
+    )
+    var data = await response.json()
+    var montoACambiar = parseFloat(monto.value)
     var totalLemon = (
       parseFloat(data.totalBid) *
       (montoACambiar - comisionCrypto)
-    );
+    )
     if (!montoACambiar || Number.isNaN(montoACambiar) || montoACambiar < 20) {
       document.querySelector("#total-lemon").innerText = `
-      El monto minimo para transferir desde Ontop es $20`;
-      return null;
+      El monto minimo para transferir desde Ontop es $20`
+      return null
     }
     document.querySelector("#total-lemon").innerText = `
               Dolar Crypto = $${data.totalBid.toFixed(2)}
               TOTAL = $${totalLemon.toFixed(2)}
               Estarias cambiando a = $${(
-                totalLemon / montoACambiar
-              ).toFixed(2)}
-              `;
+        totalLemon / montoACambiar
+      ).toFixed(2)}
+              `
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-async function getDolarBlue() {
+async function getDolarBlue () {
   try {
     const data = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
     const dolares = await data.json()
     const filteredDolar = dolares.find((e) => e.casa.nombre === 'Dolar Blue')?.casa.compra
-    const dolarBlue = parseFloat(filteredDolar.replace(",", ".")).toFixed(2);
-    let montoACambiar = parseFloat(monto.value);
+    const dolarBlue = parseFloat(filteredDolar.replace(",", ".")).toFixed(2)
+    let montoACambiar = parseFloat(monto.value)
     console.log(Number.isNaN(montoACambiar))
-    if (!montoACambiar || Number.isNaN(montoACambiar)  || montoACambiar < 1) {
+    if (!montoACambiar || Number.isNaN(montoACambiar) || montoACambiar < 1) {
       document.querySelector("#total-blue").innerText = `
       Monto invalido
-      `;
+      `
       return null
     }
     document.querySelector("#total-blue").innerText = `
     Dolar Blue = $${dolarBlue}
     TOTAL = $${(dolarBlue * montoACambiar).toFixed(2)}
     Estarias cambiando a = $${dolarBlue}
-    `;
+    `
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 calcular.addEventListener("click", function (e) {
-  e.preventDefault();
-  getDolarCCL();
-  getDolarCrypto();
-  getDolarBlue();
-});
+  e.preventDefault()
+  getDolarCCL()
+  getDolarCrypto()
+  getDolarBlue()
+})
